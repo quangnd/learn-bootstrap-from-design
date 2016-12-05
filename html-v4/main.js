1c506beca10816bcfcfaa23efa37445d0cb1f5d6
quangnd <quangnd.edu@gmail.com>
2016-12-05 18:40:24 +0700

    Update menu process

--- html-v4/main.js
+++ html-v4/main.js
@@ -6,4 +6,19 @@ $(document).ready(function($) {
     $('#newsCarousel').carousel({
         interval: 10000
     })
+
+    $('li.homeMenu').addClass('liActive');
+    $('li.menuItem').hover(function(){
+         //disable hover effect on active menu item
+         if (!$(this).hasClass('liActive')) {
+             $(this).toggleClass('liHover');
+         }
+    })
+
+    $('li.menuItem').click(function(){
+         $('li.menuItem').removeClass('liActive');
+         $('li.menuItem').removeClass('liHover');
+         
+         $(this).addClass('liActive');
+    })
 });
